9c346dfcd99ce507713777f6bd974e2e4f60a483
Alejandro Hernández <picanteverde@gmail.com>
2013-03-23 07:07:03 -0300

    common configs

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 var express = require('express'),
 	app = express(),
-	port = process.env.PORT || 5000;
+	port = process.env.PORT || 5000.
+	public_dir = process.env.PUBLIC_DIR || "public";
+
+app.configure(function(){
+
+	app.use(express.favicon());
+	app.use(express.logger("dev"));
+
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.methodOverride());
+
+	app.use(app.router);
+
+	app.use(express["static"](public_dir));
+
+});
+
+app.configure("development", function() {
+    app.use(express.errorHandler({
+        dumpException: true,
+        showStack: true
+    }));
+});
 
 app.get('/', function(req, res){
   var body = 'Hello World';
