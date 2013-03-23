e40d710d70dba439befd40c3350c9fca0f42d290
Alejandro Hernández <picanteverde@gmail.com>
2013-03-23 07:01:26 -0300

    adding a route

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 var express = require('express'),
 	app = express();
+
+app.get('/', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
