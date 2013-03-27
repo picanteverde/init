26d9b1f92d524fa510b8945dab3b8479ad655978
Alejandro Hernández <picanteverde@gmail.com>
2013-03-27 18:00:58 -0300

    adding db clients for components initialization

--- routes.js
+++ routes.js
@@ -2,7 +2,8 @@
 	var simple = require("./components/simple/routes.js");
 
 	module.exports = {
-		load: function(app){
+		load: function(app, dbs){
+			
 			app.get("/hello", simple.helloWorld);
 		}
 	};
