2c68625de39a62339cc9813f92f2a531dc60efa4
Alejandro Hernández <picanteverde@gmail.com>
2013-03-27 13:35:29 -0300

    removing variables

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var config = require("./config.json"),
 	express = require("express"),
 	routes = require("./routes.js"),
-	app = express(),
-	port = process.env.PORT || config.server.port,
-	public_dir = process.env.PUBLIC_DIR || config.server.public_dir;
+	app = express();
+	
+config.server.port = process.env.PORT || config.server.port;
+config.server.public_dir = process.env.PUBLIC_DIR || config.server.public_dir;
 
 app.configure(function(){
 
@@ -16,7 +17,7 @@ app.configure(function(){
 
 	app.use(app.router);
 
-	app.use(express["static"](public_dir));
+	app.use(express["static"](config.server.public_dir));
 
 });
 
@@ -29,5 +30,5 @@ app.configure("development", function() {
 
 routes.load(app);
 
-app.listen(port);
-console.log("App listening on port: " + port);
+app.listen(config.server.port);
+console.log("App listening on port: " + config.server.port);
